d671c35eb38d435b730aa5c9cd90d2fe8b142c4a
Milica Sucevic <milicasucevic@gmail.com>
2022-11-15 11:29:16 +0000

    initial commit

---
+++ networks.tf
@@ -0,0 +1,9 @@
+#Create VPC in us-east-1
+resource "aws_vpc" "vpc_master" {
+  provider             = aws.region-master
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+  tags = {
+    Name = "master-vpc-jenkins"
+  }
