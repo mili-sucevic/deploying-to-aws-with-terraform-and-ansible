74bf8dfead982edef96e05b04bac60fead7da267
Milica Sucevic <milicasucevic@gmail.com>
2022-11-17 17:42:32 +0000

    fix typo in us-west-2 with depends on

--- instances.tf
+++ instances.tf
@@ -70,9 +70,10 @@ resource "aws_instance" "jenkins-worker-oregon" {
 
   provisioner "local-exec" {
     command = <<EOF
-aws --profile ${var.profile} ec2 wait instance-status-ok --region ${var.region-master} --instance-ids ${self.id}
-ansible-playbook --extra-vars 'passed_in_hosts=tag_Name_${self.tags.Name}' ansible_templates/jenkins-master-sample.yml
+aws --profile ${var.profile} ec2 wait instance-status-ok --region ${var.region-worker} --instance-ids ${self.id}
+ansible-playbook --extra-vars 'passed_in_hosts=tag_Name_${self.tags.Name}' ansible_templates/jenkins-worker-sample.yml
 EOF
   }
 
 }
+
