6383cb2cce1cc55b8ad5dd2bd39c2b5bd829c4b5
Milica Sucevic <milicasucevic@gmail.com>
2022-11-21 08:30:51 +0000

    dns config

--- outputs.tf
+++ outputs.tf
@@ -1,6 +1,18 @@
 ###This chunk of template can also be put inside outputs.tf for better segregation 
+output "amiId-us-east-1" {
+  value = data.aws_ssm_parameter.linuxAmi.value
+}
+
+output "amiId-us-west-2" {
+  value = data.aws_ssm_parameter.linuxAmiOregon.value
+}
+
 output "Jenkins-Main-Node-Public-IP" {
   value = aws_instance.jenkins-master.public_ip
+}
+
+output "Jenkins-Main-Node-Private-IP" {
+  value = aws_instance.jenkins-master.private_ip
 }
 
 output "Jenkins-Worker-Public-IPs" {
@@ -10,6 +22,17 @@ output "Jenkins-Worker-Public-IPs" {
   }
 }
 
+output "Jenkins-Worker-Private-IPs" {
+  value = {
+    for instance in aws_instance.jenkins-worker-oregon :
+    instance.id => instance.private_ip
+  }
+}
+
 output "LB-DNS-NAME" {
   value = aws_lb.application-lb.dns_name
+}
+
+output "url" {
+  value = aws_route53_record.jenkins.fqdn
 }
